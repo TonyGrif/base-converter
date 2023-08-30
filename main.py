a96f3525f2e14cce37e71b714ff2cfa19339fe9b
TonyGrif <TonyGriffin2000@gmail.com>
2023-08-30 16:34:13 -0400

    CLI Argument Parser (#2)

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
+
 def main():
-    print("Hello World!")
+    parser = argparse.ArgumentParser(
+            prog="Decimal to Binary Converter",
+            description="A python script to convert decimal integers and floats to their binary equivalent.")
+
+    parser.add_argument(
+            'decimals',
+            nargs='+',
+            help="Decimal number(s) to be converted to binary.")
+
+    args = parser.parse_args()
+
+    print(args)
+
     return
 
 if __name__ == "__main__":
