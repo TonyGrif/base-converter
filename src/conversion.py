da2f572a8a8ea49c737f5d643e4e4389eeda9192
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-03 13:17:50 -0400

    initial function and testing suite

---
+++ src/conversion.py
@@ -0,0 +1,11 @@
+def convertToDecimal(num: int or float) -> str:
+    """
+    Converts an integer of float number to its binary representation.
+
+    Parameters:
+        num (int or float): The number to be converted.
+
+    Returns:
+        binStr (str): The binary string representation.
+    """
+    pass
---
+++ tests/test_conversion.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+from conversion import convertToDecimal
+
+
+class TestConverstion:
+    def test_positiveInteger(self):
+        pass
+
+    def test_negativeInteger(self):
+        pass
+
+    def test_positiveDecimal(self):
+        pass
+
+    def test_negativeDecimal(self):
+        pass
