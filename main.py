90c5be3c78c8518d7e30089ee114b553c9f45de1
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-12 18:47:00 -0400

    update main docs

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-"""The main module for the decimal-to-binary converter program.
+"""The main module for the decimal-to-base converter program.
 
-This program allows the user to input a non-zero number of real
-numbers to convert to their binary equivalent. It is assumed that
+This program allows the user to input a non-zero amount of real
+numbers to convert to their chosen base equivalent. It is assumed that
 the input is, in fact, real numbers.
 
 This file can be run as `./main.py [-h] args [args ...]`
@@ -19,13 +19,13 @@ from src.conversion import Converter
 def main():
     """Run the decimal-to-binary program."""
     parser = argparse.ArgumentParser(
-        prog="Decimal to Binary Converter",
-        description="Script to convert decimal numbers to binary.",
+        prog="Base-10 to Base-X Converter",
+        description="Script to convert decimal numbers to another base.",
         usage="./main.py [-h] args [args ...]",
     )
 
     parser.add_argument(
-        "decNum", nargs="+", help="Decimal number(s) to be converted to binary."
+        "decNum", nargs="+", help="Decimal number(s) to be converted to another base."
     )
 
     args = parser.parse_args()
