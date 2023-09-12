ce8e809d40ceb7dbd2215e0fd6d3de0f22e22f21
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-12 16:01:57 -0400

    add tests

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -9,13 +9,12 @@ def converter():
     return Converter([0.5, 0.25, 0.75])
 
 
+@pytest.fixture
+def intConverter():
+    return Converter([1, 2, 10, 16, 17])
+
+
 class TestConverstion:
-    def test_positiveInteger(self):
-        pass
-
-    def test_negativeInteger(self):
-        pass
-
     def test_positiveDecimal(self, converter):
         assert converter.conversions[0]["base-2"] == "0.1"
         assert converter.conversions[1]["base-2"] == "0.01"
@@ -34,6 +33,16 @@ class TestConverstion:
         assert converter.convert_to_binary(-0.2) == "-0.00110011"
         assert converter.convert_to_binary(-0.142857) == "-0.00100100"
 
+    def test_positiveInteger(self, intConverter):
+        assert intConverter.conversions[0]["base-2"] == "1"
+        assert intConverter.conversions[1]["base-2"] == "10"
+        assert intConverter.conversions[2]["base-2"] == "1010"
+        assert intConverter.conversions[3]["base-2"] == "10000"
+        assert intConverter.conversions[4]["base-2"] == "10001"
+
+    def test_negativeInteger(self):
+        pass
+
     def test_output(self, converter):
         assert "Base 10" in converter.output()
         assert "Base 2" in converter.output()
@@ -41,3 +50,4 @@ class TestConverstion:
         for conversion in converter.conversions:
             assert str(conversion["base-10"]) in converter.output()
             assert str(conversion["base-2"]) in converter.output()
+
