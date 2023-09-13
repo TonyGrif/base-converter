6bcd1132d7969fa7290d39d89253c4851ee198ee
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-12 22:22:40 -0400

    add argument to cli

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 """The main module for the decimal-to-base converter program.
 
-This program allows the user to input a non-zero amount of real
-numbers to convert to their chosen base equivalent. It is assumed that
-the input is, in fact, real numbers.
+This program allows the user to input a non-zero base to convert
+to and a non-zero amount of real numbers to convert to their chosen
+base equivalent. It is assumed that the input is, in fact, real numbers.
 
 This file can be run as `./main.py [-h] args [args ...]`
 """
@@ -23,6 +23,8 @@ def main():
         description="Script to convert decimal numbers to another base.",
         usage="./main.py [-h] args [args ...]",
     )
+
+    parser.add_argument("baseNum", help="Base to convert to.")
 
     parser.add_argument(
         "decNum", nargs="+", help="Decimal number(s) to be converted to another base."
