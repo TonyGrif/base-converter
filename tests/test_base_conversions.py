8357e30de6e021a9cf1c869ea33da4003ad946db
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-12 22:53:56 -0400

    update conversion and output testing suite

--- tests/test_base_conversions.py
+++ tests/test_base_conversions.py
@@ -20,8 +20,14 @@ def sixty_convert(dec_nums):
 
 
 class TestBaseConversions:
-    def test_positive_decimal(self, eight_convert):
-        pass
+    def test_positive_decimal(self, eight_convert, sixty_convert):
+        # TODO: asserts for eight
+
+        assert sixty_convert[0]["base-60"] == "0.30"
+        assert sixty_convert[1]["base-60"] == "0.15"
+        assert sixty_convert[2]["base-60"] == "0.45"
+        assert sixty_convert[3]["base-60"] == "0.48"
+        assert sixty_convert[4]["base-60"] == "0.10"
 
     def test_negative_decimal(self):
         pass
@@ -35,5 +41,15 @@ class TestBaseConversions:
     def test_floating_point(self):
         pass
 
-    def test_output(self):
-        pass
+    def test_output(self, eight_convert, sixty_convert):
+        # TODO: asserts for eight
+
+        sixty_str = sixty_convert.output()
+        assert "Base 10" in sixty_str
+        assert "Base 60" in sixty_str
+
+        for conversion in sixty_convert.conversions:
+            assert str(conversion["base-10"]) in sixty_str
+            assert str(conversion["base-60"]) in sixty_str
+
+
