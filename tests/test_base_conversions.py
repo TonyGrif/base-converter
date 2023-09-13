503677c4acaf292a99900bf04f2058b7f9600727
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-12 22:42:12 -0400

    set up new testing suite

--- tests/test_base_conversions.py
+++ tests/test_base_conversions.py
@@ -4,8 +4,23 @@ import pytest
 from conversion import Converter
 
 
+@pytest.fixture
+def dec_nums():
+    return [0.5, 0.75, 0.8, 0.16666]
+
+
+@pytest.fixture
+def eight_convert(dec_nums):
+    return Converter(8, dec_nums)
+
+
+@pytest.fixture
+def sixty_convert(dec_nums):
+    return Converter(60, dec_nums)
+
+
 class TestBaseConversions:
-    def test_positive_decimal(self):
+    def test_positive_decimal(self, eight_convert):
         pass
 
     def test_negative_decimal(self):
