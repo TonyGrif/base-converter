ffaa2033e3616645f45728c592b5268292cdd297
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-12 22:39:00 -0400

    add new base testing suite

---
+++ tests/test_base_conversions.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+from conversion import Converter
+
+
+class TestBaseConversions:
+    def test_positive_decimal(self):
+        pass
+
+    def test_negative_decimal(self):
+        pass
+
+    def test_positive_integer(self):
+        pass
+
+    def test_negative_integer(self):
+        pass
+
+    def test_floating_point(self):
+        pass
+
+    def test_output(self):
+        pass
